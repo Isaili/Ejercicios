9df2df56b65fc7850809ed12a3d42d9ba6246218
proyecto-clinica <isai123abel@gmail.com>
2025-01-22 19:24:01 -0600

    machine turing full

--- TurinMachine.py
+++ TurinMachine.py
@@ -1,53 +1,27 @@
 class TuringMachine:
     def __init__(self):
-        self.tape = []
-        self.head = 0
         self.state = 'q0'
-        self.accept_states = {'q_accept'}
-        self.reject_states = {'q_reject'}
 
-    def initialize_tape(self, input_string):
-        self.tape = list(input_string) + ['_']  
-        self.head = 0
-
-    def transition(self):
-        current_symbol = self.tape[self.head]
+    def transition(self, symbol):
         if self.state == 'q0':
-            if current_symbol == '0':
-                self.tape[self.head] = 'X'
-                self.head += 1
+            if symbol == '1':
                 self.state = 'q1'
-            elif current_symbol == '1':
-                self.tape[self.head] = 'X'
-                self.head += 1
-                self.state = 'q2'
-            elif current_symbol == '_':
-                self.state = 'q_accept'
         elif self.state == 'q1':
-            if self.tape[self.head] == '0':
-                self.head += 1
+            if symbol == '1':
                 self.state = 'q0'
-            elif self.tape[self.head] == '_':
-                self.state = 'q_reject'
-        elif self.state == 'q2':
-            if self.tape[self.head] == '1':
-                self.head += 1
-                self.state = 'q0'
-            elif self.tape[self.head] == '_':
-                self.state = 'q_reject'
 
-    def run(self):
-        while self.state not in self.accept_states and self.state not in self.reject_states:
-            self.transition()
-        return self.state
+    def run(self, input_string):
+        for symbol in input_string:
+            if symbol not in ['0', '1']:  # Si hay un carácter no válido
+                return 'q_reject'
+            self.transition(symbol)
+        return 'q_accept' if self.state == 'q0' else 'q_reject'
 
 
 input_string = input("Ingresa una cadena de 0s y 1s: ")
 
 tm = TuringMachine()
-tm.initialize_tape(input_string)
-
-result = tm.run()
+result = tm.run(input_string)
 
 if result == 'q_accept':
     print(f"La cadena '{input_string}' es aceptada por la máquina de Turing.")
